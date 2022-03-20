ef0b4b31bc3163352271ff969598d0beb7155b84
amrmoussa17 <amrmamdoh@hotmail.com>
2022-03-20 18:48:22 +0200

    revision 2 - updated sharp function and refactored code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import sharp from "sharp";
 import validator from "./utils/validator";
 const { validationResult } = require("express-validator");
@@ -14,12 +15,18 @@ app.get("/img", validator, (req: express.Request, res: express.Response) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
+  let filename = req.query.filename as string;
+  let width = req.query.width as string;
+  let height = req.query.height as string;
+  let inputPath = path.resolve("src/images");
+  let outputPath = path.resolve("src/images/cache");
+
   // resizing image using sharp module
-  sharp(__dirname + "/images/" + req.query.filename + ".jpg")
-    .resize(parseInt(req.query.width as string), parseInt(req.query.height as string))
-    .toFile(__dirname + "/images/cache/output.jpg")
-    .then((info) => {
-      res.sendFile(__dirname + "/images/cache/output.jpg");
+  sharp(`${inputPath}/${filename}.jpg`)
+    .resize(parseInt(width), parseInt(height))
+    .toFile(`${outputPath}/${filename}_${width}_${height}.jpg`)
+    .then(() => {
+      res.sendFile(`${outputPath}/${filename}_${width}_${height}.jpg`);
     })
     .catch((err) => {
       console.log(err);
